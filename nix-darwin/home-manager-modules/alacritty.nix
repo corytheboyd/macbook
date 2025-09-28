5d2e470b48eb1d40ceead27528bf240cd36e9e2e
Cory Boyd <me@corytheboyd.com>
2025-09-28 12:54:20 -0600

    Blur and copy/select

--- nix-darwin/home-manager-modules/alacritty.nix
+++ nix-darwin/home-manager-modules/alacritty.nix
@@ -6,16 +6,23 @@
       general.import = [
         "${pkgs.alacritty-theme}/share/alacritty-theme/tokyo_night.toml"
       ];
+
       font = {
         normal.family = "FiraCode Nerd Font Mono";
         size = 14;
       };
+
       window = {
         opacity = 0.95;
+        blur = true;
         padding = {
           x = 10;
           y = 10;
         };
+      };
+
+      selection = {
+        save_to_clipboard = true;
       };
     };
   };
